535a927a77e5b126166d12d440cab97c6886b5a3
advaith <advaithj1@gmail.com>
2022-09-21 16:48:36 -0700

    update types again

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,5 @@
 import { User } from './user'
+import { Channels_guild_channels } from './embed/Channels'
 import { NewMessage_message } from './embed/NewMessage'
 import { MessageUpdated_messageUpdate } from './embed/MessageUpdated'
 import { NewDirectMessage_directMessage } from './embed/NewDirectMessage'
@@ -13,29 +14,28 @@ export namespace IServer {
     signIn: User
 
     message: {
-      channel: string
+      channel: Channels_guild_channels
       message: NewMessage_message
     }
     messageUpdate: {
-      channel: string
+      channel: Channels_guild_channels
       message: MessageUpdated_messageUpdate
     }
     messageDelete: {
-      channel: string
+      channel: Channels_guild_channels
       id: string
     }
     messageDeleteBulk: {
-      channel: string
+      channel: Channels_guild_channels
       ids: string[]
     }
 
     directMessage: {
-      user: string
       message: NewDirectMessage_directMessage
     }
 
     sentMessage: {
-      channel: string;
+      channel: Channels_guild_channels;
       content: string;
       thread?: string | null;
       fileData?: string | null;
