c72e9f41031ab2531ce70f785edd1a8806c430ea
advaith <advaithj1@gmail.com>
2023-05-24 20:43:24 -0700

    updated generated channel types

--- src/embed/Channels.ts
+++ src/embed/Channels.ts
@@ -7,71 +7,21 @@
 // GraphQL query operation: Channels
 // ====================================================
 
-export interface Channels_guild_channels_TextChannel_category {
+export interface Channels_guild_channels_category {
   __typename: "Category";
   name: string;
   position: number;
 }
 
-export interface Channels_guild_channels_TextChannel {
-  __typename: "TextChannel";
+export interface Channels_guild_channels {
+  __typename: "AnnouncementChannel" | "ForumChannel" | "TextChannel" | "ThreadChannel" | "VoiceChannel";
   name: string;
   id: string;
   position: number;
-  category: Channels_guild_channels_TextChannel_category | null;
-  canSend: boolean;
   nsfw: boolean;
-}
-
-export interface Channels_guild_channels_AnnouncementChannel_category {
-  __typename: "Category";
-  name: string;
-  position: number;
-}
-
-export interface Channels_guild_channels_AnnouncementChannel {
-  __typename: "AnnouncementChannel";
-  name: string;
-  id: string;
-  position: number;
-  category: Channels_guild_channels_AnnouncementChannel_category | null;
   canSend: boolean;
-  nsfw: boolean;
+  category: Channels_guild_channels_category | null;
 }
-
-export interface Channels_guild_channels_VoiceChannel_category {
-  __typename: "Category";
-  name: string;
-  position: number;
-}
-
-export interface Channels_guild_channels_VoiceChannel {
-  __typename: "VoiceChannel";
-  name: string;
-  id: string;
-  position: number;
-  category: Channels_guild_channels_VoiceChannel_category | null;
-  canSend: boolean;
-  nsfw: boolean;
-}
-
-export interface Channels_guild_channels_ThreadChannel_category {
-  __typename: "Category";
-  name: string;
-  position: number;
-}
-
-export interface Channels_guild_channels_ThreadChannel {
-  __typename: "ThreadChannel";
-  name: string;
-  id: string;
-  position: number;
-  category: Channels_guild_channels_ThreadChannel_category | null;
-  canSend: boolean;
-  nsfw: boolean;
-}
-
-export type Channels_guild_channels = Channels_guild_channels_TextChannel | Channels_guild_channels_AnnouncementChannel | Channels_guild_channels_VoiceChannel | Channels_guild_channels_ThreadChannel;
 
 export interface Channels_guild {
   __typename: "Guild";
